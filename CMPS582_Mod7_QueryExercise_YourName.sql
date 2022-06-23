59ea9640978c2f4e5ca20849db0f5d2bcde5ff79
Jeff Seaman <46333921+jeffreyjseamanppu@users.noreply.github.com>
2022-06-23 16:53:28 -0400

    Add files via upload

---
+++ CMPS582_Mod7_QueryExercise_YourName.sql
@@ -0,0 +1,45 @@
+## Write a statement that from all your tables that selects all the values in the table.
+
+
+## Write a statement that from each of your tables that identifies 4 fields.
+
+
+## Write a statement from each of your tables that has a single where clause
+
+
+## Write a statement from each of your tables that has a IN statement with 1-3 different values
+
+
+## Write a statement from each of your tables that includes an OR statement
+
+
+## Write a statement from each of your tables that includes an order by Ascending on 1/2 of the tables, and Descending on the other 1/2
+
+
+## Write a statement from each of your tables that includes an district query for each of your table
+
+
+## Write a statement from each of your tables that includes a AND statement
+
+
+## Write a statement from each of your tables that includes a between statement
+
+
+## Write a statement from each of your tables that includes a like statement
+
+
+## Write a statement from each of your tables that includes a Limit of 5 records
+
+
+## Write a statement from each of your tables that includes a NULL statement
+
+
+## Write a statement from each of your tables that includes a INNER JOIN
+
+## Write a statement from each of your tables that includes a RIGHT JOIN
+
+## Write a statement from each of your tables that includes a LEFT JOIN
+
+## Write a statement from each of your tables that includes a UNION statement
+
+## Write a statement from each of your tables that includes a Group by statement
